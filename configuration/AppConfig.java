264d5562a812f30b2f9b7cc7aa8212151e8598d2
Iamneo <techadmin@iamneo.ai>
2023-10-02 14:32:12 +0000

    Backup Commit

--- configuration/AppConfig.java
+++ configuration/AppConfig.java
@@ -1,3 +1,23 @@
-public class AppConfig {
-    
+public class Student {
+    private int studentId;
+    private String firstName;
+    private String lastName;
+
+    // Constructors, getters, and setters
 }
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AppConfig {
+    @Bean
+    public Student student() {
+        Student student = new Student();
+        student.setStudentId(1); // Set an initial student ID
+        student.setFirstName("John");
+        student.setLastName("Doe");
+        return student;
+    }
+}
+    
+
